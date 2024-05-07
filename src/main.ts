688acc90e572302dac09755f626fd29dbdbddd7c
Mengqing <c897611977@gmail.com>
2024-05-07 21:50:52 +0800

    chore: set linter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
-import "./styles.css";
+import ElementPlus from "element-plus";
 import App from "./App.vue";
+import "./styles.css";
+import "element-plus/dist/index.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.mount("#app");
