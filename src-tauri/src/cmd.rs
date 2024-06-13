96fe9f2b99ecc15e9a92129ed16a6ad19cbd903e
ElmTran <c897611977@gmail.com>
2024-06-13 19:57:06 +0800

    update: store

---
+++ src-tauri/src/cmd.rs
@@ -0,0 +1,5 @@
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
