aa116d079fdba6da82551d77b18db188b3c29b5e
ElmTran <c897611977@gmail.com>
2024-06-26 15:26:45 +0800

    add: reload_store

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -17,7 +17,7 @@ impl AppBuidler {
         let builder = tauri::Builder
             ::default()
             .plugin(tauri_plugin_fs_watch::init())
-            .invoke_handler(tauri::generate_handler![get_windows_devices, speak])
+            .invoke_handler(tauri::generate_handler![get_windows_devices, apply, reload_store])
             .setup(|app| {
                 // define a closure to setup the app
                 HANDLE.get_or_init(|| app.handle());
