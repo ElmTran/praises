59163d4cea0acde95f06fc0c15f925b9a5219880
ElmTran <c897611977@gmail.com>
2024-07-01 20:02:48 +0800

    update: save and load tts template

--- src/store/tts.ts
+++ src/store/tts.ts
@@ -2,6 +2,7 @@ import { ref, reactive } from "vue";
 import { defineStore } from "pinia";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Buffer } from "buffer";
+import { configStore } from "./config";
 
 export const useTtsStore = defineStore("tts", () => {
   const audioUrl = ref("");
@@ -28,5 +29,30 @@ export const useTtsStore = defineStore("tts", () => {
       console.error(`Failed to convert: ${error}`);
     }
   }
-  return { audioUrl, state, convert };
+  async function saveTemplate(templateName: string) {
+    try {
+      const templates: Record<string, typeof state> | null =
+        await configStore.value.get("ttsTemplate");
+      if (templates) {
+        templates[templateName] = state;
+        await configStore.value.set("ttsTemplate", templates);
+      } else {
+        await configStore.value.set("ttsTemplate", { [templateName]: state });
+      }
+    } catch (error) {
+      console.error(`Failed to save tts config: ${error}`);
+    }
+  }
+  async function loadTemplate(templateName: string) {
+    try {
+      const templates: Record<string, typeof state> | null =
+        await configStore.value.get("ttsTemplate");
+      if (templates && templates[templateName]) {
+        Object.assign(state, templates[templateName]);
+      }
+    } catch (error) {
+      console.error(`Failed to load tts config: ${error}`);
+    }
+  }
+  return { audioUrl, state, convert, saveTemplate, loadTemplate };
 });
