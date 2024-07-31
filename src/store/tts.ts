80d64031f8a3f71167b5c3bc227f04ba3feb84ec
ElmTran <c897611977@gmail.com>
2024-07-31 18:46:41 +0800

    update: msedge api

--- src/store/tts.ts
+++ src/store/tts.ts
@@ -6,6 +6,7 @@ import { Buffer } from "buffer";
 export const useTtsStore = defineStore("tts", () => {
   const audioUrl = ref("");
   const state = reactive({
+    service: "msedge",
     text: "",
     language: "",
     speaker: "",
@@ -13,20 +14,20 @@ export const useTtsStore = defineStore("tts", () => {
     role: "",
     rate: 0,
     pitch: 0,
+    volume: 100,
   });
   async function convert() {
     try {
       const rate = state.rate.toString();
       const pitch = state.pitch.toString();
-      const payload = { ...state, rate: rate, pitch: pitch };
+      const volume = state.volume.toString();
+      const payload = { ...state, rate: rate, pitch: pitch, volume: volume };
       const resp: Uint8Array | string = await invoke("convert", payload);
       if (typeof resp === "string") {
         console.error(`convert error: ${resp}`);
         return;
       }
-      audioUrl.value = URL.createObjectURL(
-        new Blob([Buffer.from(resp)], { type: "audio/mp3" }),
-      );
+      audioUrl.value = URL.createObjectURL(new Blob([Buffer.from(resp)], { type: "audio/mp3" }));
     } catch (error) {
       console.error(`Failed to convert: ${error}`);
     }
